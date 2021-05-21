2ed50b2f1cf69510062bc09e06e13592486cbb15
Ivan Milincic <kreativan@outlook.com>
2021-05-21 12:56:40 +0200

    generate prev/next test

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="prev-next-article uk-flex uk-flex-between uk-link-heading">
     <div class="prev-article"> 
-      <nuxt-link v-if='prevPage && prevPage.path' :to='prevPage.path'>{{ prevPage.title }}</nuxt-link>
+      <nuxt-link v-if='prevPage' :to='`/blog/${prevPage.slug}/`'>{{ prevPage.title }}</nuxt-link>
     </div>
     <div class="next-article">
-       <nuxt-link v-if='nextPage && nextPage.path' :to='nextPage.path'>{{ nextPage.title }}</nuxt-link>
+       <nuxt-link v-if='nextPage' :to='`/blog/${nextPage.slug}/`'>{{ nextPage.title }}</nuxt-link>
     </div>
   </div>
 </template>
@@ -12,10 +12,6 @@
 <script>
 export default {
   props: ['prevPage', 'nextPage'],
-  created() {
-    console.log(this.prevPage)
-    console.log(this.nextPage)
-  }
 }
 </script>
 
