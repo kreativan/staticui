cf769f2f25c64ffe64f6f5996790f98a571b3ebe
Ivan Milincic <kreativan@outlook.com>
2021-05-21 13:12:30 +0200

    next-prev add arrows

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="prev-next-article uk-flex uk-flex-between uk-link-heading">
     <div class="prev-article"> 
-      <nuxt-link v-if='prev' :to='`/${root}/${prev.slug}/`'>{{ prev.title }}</nuxt-link>
+      <nuxt-link v-if='prev' :to='`/${root}/${prev.slug}/`'>
+        <i uk-icon="arrow-left"></i>
+        {{ prev.title }}
+      </nuxt-link>
     </div>
     <div class="next-article">
-       <nuxt-link v-if='next' :to='`/${root}/${next.slug}/`'>{{ next.title }}</nuxt-link>
+      <nuxt-link v-if='next' :to='`/${root}/${next.slug}/`'>
+        {{ next.title }}
+        <i uk-icon="arrow-right"></i>
+      </nuxt-link>
     </div>
   </div>
 </template>
